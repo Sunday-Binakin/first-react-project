bd80a69b9a20fa6b001bcda63bb9bdc86b5f0e72
Sunday-Binakin <sundaybinakin@gmail.com>
2024-10-01 03:05:44 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+    <nav>
+      <ul>
+        <li>HOME</li>
+        <li>AWAY</li>
+        <li>NEUTRAL</li>
+      </ul>
+    </nav>
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Discover the world</h1>
+        <h2>We offer the best services</h2>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
